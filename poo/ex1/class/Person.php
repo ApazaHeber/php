341df2d0756d06452cb36cd97a8f96e24b812f66
heber <0.apazaheber.1@gmail.com>
2023-05-17 10:42:05 -0400

    poo changes

--- poo/ex1/class/Person.php
+++ poo/ex1/class/Person.php
@@ -1,8 +1,25 @@
 <?php
 
 class Person{
-  public $name, $lastname, $years;
+  public $name, $lastname, $age;
 
-}
+  // We controller the data value with these functions
+  // Getters : Used to obtain the value, only lecture, without argument
+  // Setter : Used to modify the value , also writting, with argument
+  
+  public function setName($nombre){
+    // in this case we controller if it is uppercase or lowercase
+    $this->name = strtolower($nombre);
+  }
+  
+  //to transform the way how return & show data 
+  public function getName(){
+    return ucwords($this->name);
+  }
+
+  // CONSTRUCTOR
+
+  public function _construct(){}
 
 ?>
+
--- poo/ex1/index.php
+++ poo/ex1/index.php
@@ -2,7 +2,27 @@
 
 require_once('class/Person.php');
 
+// instanciamos un nuevo objeto
 $person1 = new Person;
 
-var_dump($person1);
+//add data to the object
+$person1->name = 'pedro';
+$person1->lastname = 'perez';
+$person1->age = 22;
+
+
+$person2 = new Person;
+
+$person2->setName('ALICIA');
+$person2->lastname = 'quispe';
+$person2->age = 20;
+
+echo "name of first person is : " . $person1->getName();
+echo '<br>';
+echo "name of second person is : " . $person2->getName();
+
+
+
+
 ?>
+
